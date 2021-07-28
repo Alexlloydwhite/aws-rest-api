4e706bed6eca18507329011fb4a763585fc6370e
Alex White <alexlloydwhite@gmail.com>
2021-07-28 15:17:40 -0500

    adding lambda function to add new todo

---
+++ src/addTodo.js
@@ -0,0 +1,34 @@
+const { v4 } = require("uuid");
+const AWS = require("aws-sdk");
+
+const addTodo = async (event) => {
+
+  const dynamodb = AWS.DynamoDB.DocumentClient()
+
+  const { todo } = JSON.parse(event.body);
+  const createdAt = new Date();
+  const id = v4();
+
+  console.log(`IN addTodo - ID: ${id}`);
+
+  const newTodo = {
+    id,
+    todo,
+    createdAt,
+    completed: false
+  }
+
+  await dynamodb.put({
+    TableName: "TodoTable",
+    Item: newTodo
+  });
+
+  return {
+    statusCode: 200,
+    body: JSON.stringify(newTodo),
+  };
+};
+
+module.exports = {
+  handler: addTodo
+}
