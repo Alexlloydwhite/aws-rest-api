575cef5b15d522e415511c034a7dfa3c56deba7a
Alex White <alexlloydwhite@gmail.com>
2021-08-13 16:19:09 -0500

    updating lamdba updateTodo

--- src/updateTodo.js
+++ src/updateTodo.js
@@ -1,37 +1,30 @@
 const AWS = require("aws-sdk");
-const middy = require("@middy/core");
-const httpJsonBodyParser = require("@middy/http-json-body-parser");
 
-const updateTodo = async (event) => {
+const updateTodo = async(event) => {
 
-  const dynamodb = new AWS.DynamoDB.DocumentClient();
+    const dynamodb = new AWS.DynamoDB.DocumentClient();
 
-  const { completed } = event.body;
-  const { id } = event.pathParameters
+    const { completed } = JSON.parse(event.body);
+    const { id } = event.pathParameters
 
-  await dynamodb.update({
-    TableName: "TodoTable",
-    Key: { id },
-    UpdateExpression: 'set completed = :completed',
-    ExpressionAttributeValues: {
-        ':completed': completed
-    },
-    ReturnValues: "ALL_NEW"
-  }).promise();
+    await dynamodb.update({
+        TableName: "TodoTable",
+        Key: { id },
+        UpdateExpression: 'set completed = :completed',
+        ExpressionAttributeValues: {
+            ':completed': completed
+        },
+        ReturnValues: "ALL_NEW"
+    }).promise();
 
-  return {
-    statusCode: 200,
-    headers: {
-      "Access-Control-Allow-Origin" : "*", // Required for CORS support to work
-      "Access-Control-Allow-Credentials" : true, // Required for cookies, authorization headers with HTTPS
-      "Access-Control-Allow-Methods": "PUT" // Allow only PUT request 
-    },
-    body: JSON.stringify({
-        msg: "Todo Updated"
-    }),
-  };
+    return {
+        statusCode: 200,
+        body: JSON.stringify({
+            msg: "Todo Updated"
+        }),
+    };
 };
 
 module.exports = {
-  handler: middy(updateTodo).use(httpJsonBodyParser())
+    handler: updateTodo
 }
